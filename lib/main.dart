656179200073eee4318fb6355212594536903780
Priyanshu Suryawanshi <ps3556911@gmail.com>
2023-10-24 10:20:36 +0530

    Added Icons

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PushUpApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PushUpApp extends StatelessWidget {
+  const PushUpApp({super.key});
 
   @override
   Widget build(BuildContext context) {
